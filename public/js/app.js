893d6593bf5bd51641e4c99e84acff8b3c0cdb97
Rythem Datta <ritzy192@gmail.com>
2021-07-20 22:02:02 +0530

    updated code to deploy to heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,13 +11,11 @@ weatherForm.addEventListener('submit', (e) => {
   messageOne.textContent = 'Fetching Weather...';
   messageTwo.textContent = '';
 
-  fetch('http://localhost:3000/weather?address=' + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) return (messageOne.textContent = data.error);
-        messageOne.textContent = data.location;
-        messageTwo.textContent = data.forcast;
-      });
-    }
-  );
+  fetch('/weather?address=' + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) return (messageOne.textContent = data.error);
+      messageOne.textContent = data.location;
+      messageTwo.textContent = data.forcast;
+    });
+  });
 });
