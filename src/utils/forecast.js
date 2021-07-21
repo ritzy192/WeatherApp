4b31b23b60198615692df456779ab5d608eaa1b8
Rythem Datta <ritzy192@gmail.com>
2021-07-21 14:15:27 +0530

    updated forcast message

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,7 +14,7 @@ const forecast = (lat, long, callback) => {
         }
         else
         {
-            callback(undefined,body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature +' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+            callback(undefined,body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature +' degrees out. It feels like ' + body.current.feelslike + ' degrees out. Humidity is ' + body.current.feelslike + '%' )
         }
     })
 }
